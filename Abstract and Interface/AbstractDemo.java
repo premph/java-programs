d76739d14092f080cc4ed7bb7a8cfeb228ca0244
Prem Prakash Hansda <prem909769@gmail.com>
2018-06-27 14:02:20 +0530

    Create AbstractDemo.java

---
+++ Abstract and Interface/AbstractDemo.java
@@ -0,0 +1,16 @@
+abstract class AbstractDemo
+{
+/**
+* Here class is abstract also method is abstract.
+* dosomething is an abstract method
+* show is a non abstract method
+*/
+abstract void dosomething();
+
+void show()
+  {
+  System.out.print("non abstract method");
+  }
+
+
+}
